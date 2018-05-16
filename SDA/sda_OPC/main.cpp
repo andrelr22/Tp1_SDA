571c86a306e35e338cced8f4e81a640acbc0ee16
squid666 <goncalves.costa@live.com>
2018-05-16 20:52:49 -0300

    Implementação thred

--- SDA/sda_OPC/main.cpp
+++ SDA/sda_OPC/main.cpp
@@ -1,10 +1,18 @@
 #include "stdafx.h"
+#include <thread>         // std::thread
+#include <string.h>
 
-void connect_OPC(void);
+
+void connect_OPC(char function[]);
 int servidor_TCP(void);
 
 int main()
 {
-	connect_OPC();
-	servidor_TCP();
+	char function1[] = "read";
+	char function2[] = "write";
+	std::thread OPC_A_READ(connect_OPC, function1);
+	std::thread OPC_S_WRITE(connect_OPC, function2);
+	std::thread TCP_SERVER(servidor_TCP);
+
+	TCP_SERVER.join();
 }
