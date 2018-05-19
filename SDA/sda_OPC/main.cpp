f4dc20f76129c7ac0982c8bb1d9f650de89cac84
squid666 <goncalves.costa@live.com>
2018-05-19 00:40:40 -0300

    ainda bosta

--- SDA/sda_OPC/main.cpp
+++ SDA/sda_OPC/main.cpp
@@ -13,11 +13,13 @@ int main()
 {
 	//strcpy_s(TCPparaOPC,sizeof(TCPparaOPC),"123456789009876543211234567");
 	int function = 0;
-	std::thread OPC_A_READ(connect_OPC, function);
+	connect_OPC(function);
+	/*std::thread OPC_A_READ(connect_OPC, function);
 	function = 1;
 	std::thread OPC_S_WRITE(connect_OPC, function);
 
 	std::thread TCP_SERVER(servidor_TCP);
 
 	TCP_SERVER.join();
+*/
 }
