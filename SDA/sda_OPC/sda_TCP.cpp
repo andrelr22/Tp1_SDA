a53c5b53ad31353b61f03a02ea9eeb5407784c60
andrelr22 <andrelagerocha@gmail.com>
2018-05-20 21:55:14 -0300

    TA PRONTO CARAI

--- SDA/sda_OPC/sda_TCP.cpp
+++ SDA/sda_OPC/sda_TCP.cpp
@@ -1,4 +1,4 @@
-// TCP.cpp : define o ponto de entrada para o aplicativo do console.
+// sda_TCP.cpp : define o ponto de entrada para o aplicativo do console.
 //
 
 #include "stdafx.h"
@@ -33,15 +33,6 @@ int ApontadorPosicao = 0;
 
 void connect_OPC(int function);
 
-void FuncaoDebug(int id, char  *Buffer) {
-	int posicao = (id) % 5;
-	for (int i = 0; i < TAMBUFFER1; i++) {
-		OPCparaTCPV[posicao][i] = Buffer[i];
-		printf("%c", OPCparaTCPV[posicao][i]);
-	}
-	printf("\n");
-}
-
 
 
 void leBuffer(int id, char *idstring, char  *Buffer) {
@@ -53,12 +44,11 @@ void leBuffer(int id, char *idstring, char  *Buffer) {
 
 void adicionaAoBuffer(int id,char  *Buffer) {
 	int posicao =0;
-	printf("a string adicionada ao buffer, eh:");
 	for (int i = 0; i < TAMBUFFER1; i++) {
 		TCPparaOPCV[posicao][i]= Buffer[i] ;
-		printf("%c", TCPparaOPCV[posicao][i]);
+
 	}
-	printf("\n");
+
 
 }
 
@@ -69,12 +59,12 @@ int enviaACK(SOCKET ClientSocket, int IdMsg) {
 	strcat_s(sendbuf, sizeof(sendbuf), Idstring);
 	int iSendResult = send(ClientSocket, sendbuf, strlen(sendbuf), 0);
 	if (iSendResult == SOCKET_ERROR) {
-		printf("send failed with error: %d\n", WSAGetLastError());
+		printf("SERVIDOR TCP: Erro no envio de mensagens: %d\n", WSAGetLastError());
 		closesocket(ClientSocket);
 		WSACleanup();
 		return 1;
 	}
-	printf("SERVIDOR TCP: ACK enviado\n");
+	printf("SERVIDOR TCP: ACK enviado: %s \n",sendbuf);
 	return 0;
 }
 
@@ -86,11 +76,6 @@ char* separaString(char *String, int tamanho) {
 	return StringRetorno;
 }
 
-//int enviaParametros(const char *Buffer, int IdMensagem) {
-//char Parametros[50];
-
-
-//}
 
 
 
@@ -105,23 +90,24 @@ int enviaDados(SOCKET ClientSocket, const char *Buffer, int IdMensagem) {
 	strcat_s(BufferEnvio, sizeof(BufferEnvio), Buffer);
 
 	int iSendResult = send(ClientSocket, BufferEnvio, strlen(BufferEnvio), 0);
-	printf("SERVIDOR TCP: o buffer enviado para o MES foi %s\n", BufferEnvio);
-
+	printf("SERVIDOR TCP: Mensagem Enviada para o MES : %s\n", BufferEnvio);
+	printf("SERVIDOR TCP: Numero de Bytes enviados: %d\n", iSendResult);
 	if (iSendResult == SOCKET_ERROR) {
-		printf("send failed with error: %d\n", WSAGetLastError());
+		printf("SERVIDOR TCP: Erro ao enviar mensagem: %d\n", WSAGetLastError());
 		closesocket(ClientSocket);
 		WSACleanup();
 		return 1;
 	}
-	printf("Bytes sent: %d\n", iSendResult);
+	printf("SERVIDOR TCP: Numero De Bytes enviados: %d\n", iSendResult);
 	int recvbuflen = DEFAULT_BUFLEN;
 	CHAR recvbuf[DEFAULT_BUFLEN];
 	iSendResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
 	if (strncmp(&recvbuf[0], "11", 2) == 0) {
-		printf("ACK recebido\n");
+		recvbuf[iSendResult] = '\0';
+		printf("SERVIDOR TCP: mensagem de ACK recebida : %s\n", recvbuf);
 	}
 	else {
-		printf("ACK não recebido, encerrando servidor\n");
+		printf("SERVIDOR TCP: ACK não recebido, encerrando servidor\n");
 		exit(0);
 	}
 	return 0;
@@ -135,28 +121,8 @@ int enviaDados(SOCKET ClientSocket, const char *Buffer, int IdMensagem) {
 int servidor_TCP(void)
 {
 	
-	///////////PARTE DE TESTE ///////////////
-	char test[35], number[4];
-	strcpy_s(test, sizeof(test), "this is a test1");
-	strcpy_s(number, sizeof(number), "003");
-	
-	adicionaAoBuffer(3, test);
-	printf(" aa%c", TCPparaOPCV[2][3]);
 
-	strcpy_s(test, sizeof(test), "000;000;00000;False;000;0000000.00");
-	FuncaoDebug(0, test);
 
-	strcpy_s(test, sizeof(test), "001;001;00001;False;000;0000000.00");
-	FuncaoDebug(1, test);
-
-	strcpy_s(test, sizeof(test), "002;002;00002;False;000;0000000.00");
-	FuncaoDebug(2, test);
-
-	strcpy_s(test, sizeof(test), "003;003;00003;False;000;0000000.00");
-	FuncaoDebug(3, test);
-
-	strcpy_s(test, sizeof(test), "004;004;00004;False;000;0000000.00");
-	FuncaoDebug(4, test);
 	WSADATA wsaData;
 	int iResult;
 
@@ -172,7 +138,7 @@ int servidor_TCP(void)
 
 	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if (iResult != 0) {
-		printf("WSAStartup failed with error: %d\n", iResult);
+		printf("SERVIDOR TCP: Erro na iniciação WSAStartup, erro: %d\n", iResult);
 		return 1;
 	}
 
@@ -182,27 +148,25 @@ int servidor_TCP(void)
 	hints.ai_protocol = IPPROTO_TCP;
 	hints.ai_flags = AI_PASSIVE;
 
-	// Resolve the server address and port
 	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
 	if (iResult != 0) {
-		printf("getaddrinfo failed with error: %d\n", iResult);
+		printf("SERVIDOR TCP: erro getaddrinfo : %d\n", iResult);
 		WSACleanup();
 		return 1;
 	}
 
-	// Create a SOCKET for connecting to server
 	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
 	if (ListenSocket == INVALID_SOCKET) {
-		printf("socket failed with error: %ld\n", WSAGetLastError());
+		printf("SERVIDOR TCP: Erro de socket: %ld\n", WSAGetLastError());
 		freeaddrinfo(result);
 		WSACleanup();
 		return 1;
 	}
 
-	// Setup the TCP listening socket
+	
 	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
 	if (iResult == SOCKET_ERROR) {
-		printf("bind failed with error: %d\n", WSAGetLastError());
+		printf("SERVIDOR TCP: Erro de bind: %d\n", WSAGetLastError());
 		freeaddrinfo(result);
 		closesocket(ListenSocket);
 		WSACleanup();
@@ -210,28 +174,25 @@ int servidor_TCP(void)
 	}
 
 	freeaddrinfo(result);
-	std::cout << "Aguardando Conexões";
+	std::cout << "SERVIDOR TCP: Aguardando Conexoes\n";
 	iResult = listen(ListenSocket, SOMAXCONN);
 	if (iResult == SOCKET_ERROR) {
-		printf("listen failed with error: %d\n", WSAGetLastError());
+		printf("SERVIDOR TCP: Erro listen: %d\n", WSAGetLastError());
 		closesocket(ListenSocket);
 		WSACleanup();
 		return 1;
 	}
 
-	// Accept a client socket
 	ClientSocket = accept(ListenSocket, NULL, NULL);
 	if (ClientSocket == INVALID_SOCKET) {
-		printf("accept failed with error: %d\n", WSAGetLastError());
+		printf("SERVIDOR TCP: Erro accept : %d\n", WSAGetLastError());
 		closesocket(ListenSocket);
 		WSACleanup();
 		return 1;
 	}
 	printf("SERVIDOR TCP: Conexão Estabelecida\n");
-	// No longer need server socket
 	closesocket(ListenSocket);
 	int  IdMensagem, IdLeitura = 0, IdEscrita = 0;
-	// Receive until the peer shuts down the connection
 	char sendbuf[100], IdstringTrocaDados[4];
 	while (1 == 1) {
 
@@ -239,27 +200,23 @@ int servidor_TCP(void)
 		char *strId;
 		strId = separaString((recvbuf + 3), 6);
 		IdMensagem = atoi(strId);
+		recvbuf[iResult] = '\0';
 
-
-		//printf("SERVIDOR TCP:mensagem recebida: %s \n", recvbuf);
+		printf("SERVIDOR TCP: Mensagem recebida do MES: %s \n", recvbuf);
 		if (iResult > 0) {
-			printf("Bytes received: %d\n", iResult);
+			printf("SERVIDOR TCP: Bytes recebidos: %d\n", iResult);
 
 			if (strncmp(&recvbuf[0], "00", 2) == 0) {
 				IdMensagem = IdMensagem + 1;
 				enviaACK(ClientSocket, IdMensagem);
-				printf("recvbuf eh %s\n, strlen de rcvbuf +10 eh %d\n", (recvbuf + 10),strlen(recvbuf + 10));
 				
 				
 				sprintf_s(IdstringTrocaDados, "%03d", IdEscrita);
 				strcpy_s(sendbuf, sizeof(sendbuf), "0;");
 				strcat_s(sendbuf, sizeof(sendbuf), IdstringTrocaDados);
 				strcat_s(sendbuf, sizeof(sendbuf), ";");
-				printf("antes concatenacao\n");
 				strcat_s(sendbuf, sizeof(sendbuf), (recvbuf + 10));
-				printf("antes adiciona escrita\n");
 				adicionaAoBuffer(IdEscrita, sendbuf);
-				printf("antes connectopc\n");
 				IdEscrita = IdEscrita + 1;
 				connect_OPC(1);
 		
@@ -274,7 +231,6 @@ int servidor_TCP(void)
 				sprintf_s(IdstringTrocaDados, "%03d", IdEscrita);
 				strcat_s(sendbuf, sizeof(sendbuf), IdstringTrocaDados);
 				strcat_s(sendbuf, sizeof(sendbuf), ";");
-				//adicionaAoBuffer(IdEscrita, sendbuf);
 				IdEscrita = IdEscrita + 1;
 
 				sprintf_s(IdstringTrocaDados, "%03d", IdLeitura);
@@ -285,15 +241,11 @@ int servidor_TCP(void)
 				IdMensagem = IdMensagem + 1; //referente ao ACK que será enviado pelo cliente
 
 			}
-			else if (strncmp(&recvbuf[0], "11", 2) == 0) {
-				//ack(recvbuf + 2, iResult);
-			}
-			else {
-				printf("SERVIDOR TCP:Comando Passado pelo cliente é inválido\n");
-			}
-			//system("PAUSE");
 
-			// Echo the buffer back to the sender
+			else {
+				printf("SERVIDOR TCP: Comando Passado pelo cliente é inválido\n");
+			}
+
 
 		}
 
